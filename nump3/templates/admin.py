396001f91ddb648581225aec2eee72d02a32f0d2
Will Farley <will@django.nu>
2014-03-21 17:17:40 -0600

    adds admin urls

---
+++ nump3/templates/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Mp3
+
+admin.site.register(Mp3)
---
+++ nump3/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url
+
+from .views import Mp3List, Mp3Detail
+
+
+
+urlpatterns = patterns('',
+	url(r'^$', Mp3List.as_view(), name='list'),
+	# allows slugs to contain [_, -]
+	#url(r'^(?P<slug>[-_\w]+)/$', Mp3Detail.as_view(), name='detail'),
+	# allows anything to be a slug
+	url(r'^/(?P<page_slug>.*)/(?P<slug>.*)/$', Mp3Detail.as_view(), name='detail'),
+	
+)
